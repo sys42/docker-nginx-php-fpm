7cdefffb185af973c1beceb409421e52c1867c9b
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-08-22 16:14:15 +0200

    minor improvements

--- load-testing.sh
+++ load-testing.sh
@@ -2,11 +2,13 @@
 
 if [ $# -eq 0 ]; then
   echo "USAGE: $0 url-to-test"
+  echo "loops forever using curl to fetch data from given url"
   echo
   echo "example: $0 http://localhost:30000/info.php"
   exit 1
 fi
 
+echo "url = $1"
 echo "[press Ctrl+C to stop]"
 echo
 
@@ -16,20 +18,15 @@ calls=0
 bytes=0
 page="$1"
 
-
 starttime=$(($(date +%s%N)/1000000))
-
 
 getStatistics () {
    millis=$(($(date +%s%N)/1000000 - starttime))
-   runtime=$(awk "BEGIN {printf \"%.2f\",${millis}/1000}")
-
-   ## may run into div zero error -> i don't care ...
-   callspersec=$(awk "BEGIN {printf \"%.2f\",${calls}/${runtime}}") 2>&1 >/dev/null
-   kbpersec=$(awk "BEGIN {printf \"%.2f\",${bytes}/(${runtime}*1024)}") 2>&1 >/dev/null
-   mbs=$(awk "BEGIN {printf \"%.2f\",${bytes}/(1024*1024)}") 2>&1 >/dev/null
-
-
+   runtime=$(awk "BEGIN {printf \"%.2f\",${millis}/1000}" 2>/dev/null)
+   ## may run into div zero errors -> i don't care ...
+   callspersec=$(awk "BEGIN {printf \"%.2f\",${calls}/${runtime}}" 2>/dev/null)
+   kbpersec=$(awk "BEGIN {printf \"%.2f\",${bytes}/(${runtime}*1024)}" 2>/dev/null)
+   mbs=$(awk "BEGIN {printf \"%.2f\",${bytes}/(1024*1024)}" 2>/dev/null)
    echo "#$calls | $callspersec #/s | $kbpersec kB/s | time: ${runtime}s | i/o: $mbs MB"
 }
 
@@ -39,12 +36,9 @@ ctrl_c () {
    exit 0
 }
 
-
 while true; do
   _bytes=$(curl -s "$page" | wc --bytes)
   bytes=$((bytes+_bytes))
   calls=$((calls+1))
-  if [ $(( calls % 10 )) -eq 0 ]; then      
-     echo -en "\033[s$(getStatistics)\033[u"
-  fi
+  [ $(( calls % 10 )) -eq 0 ] && echo -en "\033[s$(getStatistics)\033[u"
 done
